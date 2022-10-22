71443cfcd2d54f51996e60c95a7f71b9846c478d
Sylvia <923604368@qq.com>
2022-10-22 21:44:12 +0800

    加入了web3

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import Vue from "vue";
+import App from "./App.vue";
+import Web3 from "web3";
+Vue.config.productionTip = false;
+Vue.prototype.Web3 = Web3;
+createApp(App).mount("#app");
